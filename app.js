267cccd02f70d811bcc68869ee3d4b400f8a1ed8
priyanshu Chaudhary <priyanshuchaudhary@priyanshus-MacBook-Pro.local>
2021-12-20 15:47:16 +0530

    service changed

--- app.js
+++ app.js
@@ -15,45 +15,13 @@ app.use(cors());
 
 app.post('/sendmailCareer', async (req, res) => {
     const { ask, name, email, text, url } = await req.body;
-    // let transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: process.env.SENDER_MAIL || 'asdf',
-    //         pass: process.env.SENDER_PASS || 'sdf',
-    //     },
-    // });
-
-    //     array.push(url);
-
-    // array.forEach(function (data) {
-    //         temp.push({
-    //             path: data,
-    //             filename: 'resume.pdf'
-    //         });
-    sgMail.setApiKey("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")
+    sgMail.setApiKey(process.env.SGAPIKEY)
     var mailOptions = {
-        // from: process.env.SENDER_MAIL,
-        // to: process.env.CAREER_MAIL,
-        from: "divyalana6@gmail.com",
-        to: "priyanshuchaudhary148@gmail.com",
+        from: process.env.SENDER_MAIL,
+        to: process.env.CAREER_MAIL,
         subject: `Career: ${name}`,
-        text: `${ask}\nEmail: ${email}\nMessage: ${text} \n Resume: ${url}`,
-        // attachments: [
-        //     {
-        //         href: `${url}`
-        //     }
-        // ],
-        // attachments: temp,
+        text: `${ask}\nEmail: ${email}\nMessage: ${text} \nResume: ${url}`,
     }
-    // transporter.sendMail(mailOptions, function (error, info) {
-    //     if (error) {
-    //         //   console.log(error);
-    //         return res.send({ success: false });
-    //     } else {
-    //         // console.log('Email sent: ' + info.response);
-    //         return res.send({ success: true });
-    //     }
-    // });
     sgMail.send(mailOptions)
         .then(() => {
             return res.send({ success: true });
@@ -70,19 +38,15 @@ app.post('/sendmailContact', async (req, res) => {
     // let transporter = nodemailer.createTransport({
     //     service: 'gmail',
     //     auth: {
-    //         // host: "herokuapp",
-    //         port: 465,
     //         secure: true,
     //         user: process.env.SENDER_MAIL || 'someothermail',
     //         pass: process.env.SENDER_PASS || 'temperary-password',
     //     },
     // });
-    sgMail.setApiKey("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")
+    sgMail.setApiKey(process.env.SGAPIKEY)
     var mailOptions = {
-        from: "divyalana6@gmail.com",
-        to: "priyanshuchaudhary148@gmail.com",
-        // from: process.env.SENDER_MAIL,
-        // to: process.env.CONTACT_MAIL,
+        from: process.env.SENDER_MAIL,
+        to: process.env.CONTACT_MAIL,
         subject: `Contact: ${name}`,
         text: `${ask}\nEmail: ${email}\nMessage: ${text}`,
     }
