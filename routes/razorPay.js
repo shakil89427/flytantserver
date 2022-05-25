d90abde5fa3ff8b703fcc0e34839b535d93493d0
shakil89427 <shakilahmed89427@gmail.com>
2022-05-25 15:20:39 +0600

    razorpay done

--- routes/razorPay.js
+++ routes/razorPay.js
@@ -5,18 +5,19 @@ require("dotenv").config();
 
 router.post("/createpayment", async (req, res) => {
   try {
+    const { ammount, currency, notes } = req.body;
     const instance = new Razorpay({
       key_id: process.env.RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_KEY_SECRET,
     });
     const options = {
-      amount: 50000,
-      currency: "INR",
-      receipt: "order_rcptid_11",
+      amount: ammount * 100,
+      currency,
+      notes,
     };
-    const response = await instance.orders.create(options);
-    if (response.id) {
-      res.send(response);
+    const { id } = await instance.orders.create(options);
+    if (id) {
+      res.send({ id });
     } else {
       res.status(400).send("Something went wrong");
     }
