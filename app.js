cea6f39bcb9c8f831f03d4345986d560ad66aa29
shakil89427 <shakilahmed89427@gmail.com>
2022-05-26 21:41:09 +0600

    localhost added

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://flytant-cb72e.firebaseapp.com", "https://flytant.com"],
+    origin: [
+      "https://flytant-cb72e.firebaseapp.com",
+      "https://flytant.com",
+      "http://localhost:3000",
+    ],
   })
 );
 
