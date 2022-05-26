0482d109ae3a1c48636e84717e51787729a68d6b
shakil89427 <shakilahmed89427@gmail.com>
2022-05-26 21:00:54 +0600

    cors origin added

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ const razorpay = require("./routes/razorPay");
 /* Middlewares */
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://flytant-cb72e.firebaseapp.com",
+      "https://flytant.com",
+      "http://localhost:3000",
+    ],
+  })
+);
 
 /* Basic route for test */
 app.get("/", (req, res) => {
