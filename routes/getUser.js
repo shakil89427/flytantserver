2b0a30154d1c487cb2338bc7b0e8fc848970c432
shakil89427 <shakilahmed89427@gmail.com>
2022-06-22 12:20:24 +0600

    data changed

--- routes/getUser.js
+++ routes/getUser.js
@@ -8,7 +8,17 @@ router.post("/getuser", async (req, res) => {
       .collection("users")
       .doc(req.body.userId)
       .get();
-    res.send(userData.data());
+    const { socialCardEnabled, socialCard, username } = userData.data();
+    if (socialCardEnabled) {
+      if (!socialCard?.name || socialCard?.name === "") {
+        const newData = { ...socialCard, name: username };
+        res.send(newData);
+      } else {
+        res.send(socialCard);
+      }
+    } else {
+      res.status(404).send("Not found");
+    }
   } catch (err) {
     res.status(404).send("Not found");
   }
