5c252e933cbc8a0937079633f1784a34bb810457
shakil89427 <shakilahmed89427@gmail.com>
2022-07-05 16:40:06 +0600

    twitter api fixed

--- routes/twitterSearch.js
+++ routes/twitterSearch.js
@@ -3,20 +3,71 @@ const router = express.Router();
 const Twitter = require("twitter");
 
 router.post("/twittersearch", async (req, res) => {
+  /* Get Tweets */
+  const getTweets = async (
+    consumerKey,
+    consumerSecret,
+    token,
+    tokenSecret,
+    userData
+  ) => {
+    try {
+      const client = new Twitter({
+        consumer_key: consumerKey,
+        consumer_secret: consumerSecret,
+        access_token_key: token,
+        access_token_secret: tokenSecret,
+      });
+
+      const response = await client.get(
+        `https://api.twitter.com/2/users/${userData.id}/tweets`,
+        {
+          expansions: "attachments.media_keys",
+          "media.fields": "media_key,type,url",
+          "tweet.fields": "attachments,created_at,public_metrics",
+        }
+      );
+      const tweets = response?.data;
+      const media = response?.includes?.media;
+      res.send({ ...userData, tweets, media });
+    } catch (err) {
+      res.status(404).send("Not found");
+    }
+  };
+
+  /* Get Userdata */
+  const getUserData = async (
+    consumerKey,
+    consumerSecret,
+    token,
+    tokenSecret
+  ) => {
+    try {
+      const client = new Twitter({
+        consumer_key: consumerKey,
+        consumer_secret: consumerSecret,
+        access_token_key: token,
+        access_token_secret: tokenSecret,
+      });
+
+      const response = await client.get(
+        `https://api.twitter.com/2/users/by/username/${req.body.username}`,
+        {
+          "user.fields":
+            "description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,public_metrics,url,username,verified,withheld",
+        }
+      );
+      getTweets(consumerKey, consumerSecret, token, tokenSecret, response.data);
+    } catch (err) {
+      res.status(404).send("Not found");
+    }
+  };
+
+  /* Get tokens */
   try {
     const { consumerKey, consumerSecret, token, tokenSecret } =
       await JSON.parse(req.secrets.twitter_tokens.defaultValue.value);
-    const client = new Twitter({
-      consumer_key: consumerKey,
-      consumer_secret: consumerSecret,
-      access_token_key: token,
-      access_token_secret: tokenSecret,
-    });
-    const response = await client.get("statuses/user_timeline.json", {
-      user_id: req.body.id,
-      count: 20,
-    });
-    res.send(response);
+    getUserData(consumerKey, consumerSecret, token, tokenSecret);
   } catch (err) {
     res.status(404).send("Not found");
   }
