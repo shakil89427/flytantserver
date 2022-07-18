22eac6f066d15108b675a1513e4ee5340035a0d6
shakil89427 <shakilahmed89427@gmail.com>
2022-07-18 18:48:28 +0600

    api changed

--- routes/influencersInstagram.js
+++ routes/influencersInstagram.js
@@ -4,66 +4,82 @@ const axios = require("axios");
 const moment = require("moment");
 
 router.post("/influencersinstagram", async (req, res) => {
-  try {
-    const {
-      data: {
-        data: { user },
-      },
-    } = await axios.get(
-      `https://i.instagram.com/api/v1/users/web_profile_info/?username=${req.body.username}`,
-      {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)",
-        },
-      }
-    );
-    const {
-      edge_follow,
-      edge_followed_by,
-      edge_owner_to_timeline_media: { edges },
-      profile_pic_url,
-    } = user;
+  /* Fetch Images */
+  const getImage = async (url, updatedData) => {
+    try {
+      const { data } = await axios.get(url, {
+        responseType: "arraybuffer",
+      });
+      const image = Buffer.from(data, "binary").toString("base64");
+      updatedData.image = image;
+      res.send(updatedData);
+    } catch (err) {
+      res.send(updatedData);
+    }
+  };
 
+  /* Modify Data */
+  const modifyData = (user) => {
     let totalLikes = 0;
     let totalComments = 0;
     let totalPost = 0;
     let dates = [];
     let postPerWeek = 0;
-
-    edges?.forEach((item) => {
-      const { edge_liked_by, edge_media_to_comment, taken_at_timestamp } =
-        item?.node;
-      totalLikes = totalLikes + edge_liked_by.count || 0;
-      totalComments = totalComments + edge_media_to_comment.count || 0;
-      totalPost++;
-      dates.push(taken_at_timestamp);
+    user?.edge_owner_to_timeline_media.edges?.forEach((item) => {
+      try {
+        const { edge_liked_by, edge_media_to_comment, taken_at_timestamp } =
+          item?.node;
+        totalLikes = totalLikes + edge_liked_by.count;
+        totalComments = totalComments + edge_media_to_comment.count;
+        totalPost++;
+        dates.push(taken_at_timestamp);
+      } catch (err) {}
     });
 
-    if (dates?.length > 0) {
-      const first = moment.unix(dates[dates?.length - 1]);
-      const last = moment.unix(dates[0]);
-      const duration = last.diff(first, "milliseconds");
-      postPerWeek = Math.round((totalPost * 604800000) / duration);
-    }
-
-    const { data } = await axios.get(profile_pic_url, {
-      responseType: "arraybuffer",
-    });
-    const image = Buffer.from(data, "binary").toString("base64");
-    const finaldata = {
-      following: edge_follow.count,
-      followers: edge_followed_by.count,
+    try {
+      if (dates?.length > 0) {
+        const first = moment.unix(dates[dates?.length - 1]);
+        const last = moment.unix(dates[0]);
+        const duration = last.diff(first, "milliseconds");
+        postPerWeek = Math.round((totalPost * 604800000) / duration);
+      }
+    } catch (err) {}
+    const updatedData = {
+      following: user?.edge_follow?.count,
+      followers: user?.edge_followed_by?.count,
       totalLikes,
       totalComments,
       totalPost,
       postPerWeek,
-      image,
+      image: "",
     };
-    res.send(finaldata);
-  } catch (err) {
-    res.status(404).send("Not found");
-  }
+    getImage(user?.profile_pic_url, updatedData);
+  };
+
+  /* Fetch Data */
+  const fetchData = async () => {
+    try {
+      const {
+        data: {
+          data: { user },
+        },
+      } = await axios.get(
+        `https://i.instagram.com/api/v1/users/web_profile_info/?username=${req.body.username}`,
+        {
+          headers: {
+            "User-Agent":
+              "Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)",
+          },
+        }
+      );
+      modifyData(user);
+    } catch (err) {
+      res.status(404).send("Not found");
+    }
+  };
+
+  /* Run fetch */
+  fetchData();
 });
 
 module.exports = router;
