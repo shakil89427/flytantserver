5ceb8c065bb4db79040303e3c7d75dad70b79ae1
shakil89427 <shakilahmed89427@gmail.com>
2022-07-21 19:23:47 +0600

    db updated

--- routes/getUser.js
+++ routes/getUser.js
@@ -4,21 +4,47 @@ const admin = require("../firebase/admin");
 const firestore = admin.firestore();
 
 router.post("/getuser", async (req, res) => {
-  try {
-    const userData = await firestore
-      .collection("users")
-      .doc(req.body.userId)
-      .get();
-    const { socialCardEnabled, socialCard, username } = userData.data();
-    if (socialCardEnabled) {
-      const newData = { ...socialCard, username };
-      res.send(newData);
-    } else {
+  const getFollowData = async (card) => {
+    try {
+      const response = await firestore
+        .collection("users")
+        .doc(req.body.cardId)
+        .collection("followers")
+        .doc(req?.body?.userId)
+        .get();
+      const finalData = response.data();
+      if (finalData?.userId) {
+        res.send({ card, follow: finalData });
+      } else {
+        res.send({ card, follow: {} });
+      }
+    } catch (err) {
       res.status(404).send("Not found");
     }
-  } catch (err) {
-    res.status(404).send("Not found");
-  }
+  };
+
+  const getCardData = async () => {
+    try {
+      const response = await firestore
+        .collection("users")
+        .doc(req.body.cardId)
+        .get();
+      const { socialCardEnabled, socialCard, username } = response.data();
+      if (socialCardEnabled) {
+        const newData = { ...socialCard, username };
+        if (!req?.body?.userId) {
+          res.send({ card: newData, follow: {} });
+        } else {
+          getFollowData(newData);
+        }
+      } else {
+        res.status(404).send("Not found");
+      }
+    } catch (err) {
+      res.status(404).send("Not found");
+    }
+  };
+  getCardData();
 });
 
 module.exports = router;
